ebeb074f6732c0844fc0576d8e5297b35907e48b
Milos Nedeljkovic <dr.kiman@hotmail.com>
2016-06-13 11:54:50 -0700

    unify boilerplate

--- examples/pair-of-counters/src/main.js
+++ examples/pair-of-counters/src/main.js
@@ -1,5 +1,6 @@
+import Effects from './effects';
 import run from './boilerplate';
 
 import PairOfCounters, {View as PairOfCountersView} from './PairOfCounters';
 
-run('app', PairOfCounters.init, PairOfCounters.update, PairOfCountersView);
+run('app', () => [PairOfCounters.init(), Effects.none()], (state, action) => [PairOfCounters.update(state, action), Effects.none()], PairOfCountersView);
