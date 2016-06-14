21ff19a041fa6ad46740da056cb6b9417832a729
Milos Nedeljkovic <dr.kiman@hotmail.com>
2016-06-14 01:38:47 -0700

    remove obsolete example

--- examples/dynamic-list-of-counters/src/main.js
+++
@@ -1,5 +0,0 @@
-import run from './boilerplate';
-
-import DynamicListOfCounters from './DynamicListOfCounters';
-
-run('app', DynamicListOfCounters.init, DynamicListOfCounters.update, DynamicListOfCounters.view, DynamicListOfCounters.Msg);
